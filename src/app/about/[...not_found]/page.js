565e51d06fb8ce19507dd1e8b4690d4ae6d59f55
BabulBhat <bhatbabul.naity@gmail.com>
2025-02-08 22:38:09 +0530

    update

---
+++ src/app/about/[...not_found]/page.js
@@ -0,0 +1,7 @@
+export default function Notfound(){
+    return(
+        <div>
+            <h1>This About Page is Not Available</h1>
+        </div>
+    )
+}
---
+++ src/app/not-found.js
@@ -0,0 +1,7 @@
+export default function Notfound(){
+    return(
+        <div>
+            <h1>This Page is Not Available</h1>
+        </div>
+    )
+}
