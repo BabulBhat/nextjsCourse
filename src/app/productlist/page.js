ea01e1b4be20437f0f553640478762f0d16c4dad
BabulBhat <bhatbabul.naity@gmail.com>
2025-02-10 20:52:41 +0530

    up

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,43 +1,23 @@
-"use client"
-import { useEffect, useState } from "react";
-export default function Productlist() {
-    useEffect(() => {
-        fetchdata();
-    }, [])
-    const [product, setproduct] = useState([])
-    const fetchdata = async () => {
-        let data = await fetch(`https://dummyjson.com/products`);
-        data = await data.json();
-        setproduct(data.products);
-    }
-    return (
-        <div className="container-fluid">
-            <h1>Product List</h1>
-            <div className="row">
-                {
-                    product.map((item, index) => {
+async function productList() {
+    let data = await fetch('https://dummyjson.com/products');
+    data = await data.json();
+    return data.products;
+}
 
-                        return (
-                            <div className="col-md-4 col-lg-3 mb-4" key={index}>
-                                <div className="card">
-                                    <div className="card-body">
-                                        <div className="d-flex align-items-center justify-content-center">
-                                            <img src={item.thumbnail} className="img-fluid" />
-                                        </div>
-                                    </div>
-                                    <div className="card-footer bg-warning">
-                                        <h6 className="pb-2">{item.title}</h6>
-                                        <p>{item.description}</p>
-                                        <span className="badge bg-primary my-2">$ {item.price}</span>
-                                        <span className="badge bg-primary my-2 mx-2">{item.brand}</span>
-                                        <span className="badge bg-primary my-2 mx-2">{item.category}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
+export default async function Page() {
+    let product = await productList();
+    return (
+        <div>
+            <h2>Product List</h2>
+            {
+                product.map((item, index) => {
+                    return (
+                        <div>
+                            <h3>Name : {item.title}</h3>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
